338762b386b61976c3a9cfc9811c40eb738013ae
Sebastian <riktig@users.noreply.github.com>
2021-03-07 00:35:49 +0100

    Add exception handling to ski calculator
    Exception if too long classic ski
    Exception if unsupported ski type
    Add test for exceptions and edge cases
    Refactor ski calculator into separate functions

--- ski_calculator/calculator.py
+++ ski_calculator/calculator.py
@@ -5,14 +5,33 @@ class Skier:
         self.style = style
 
     classic = 'classic'
-    free_style = 'free_style'
+    freestyle = 'freestyle'
+
+_MAX_CLASSIC_SKI_LENGTH = 207
 
 def calculate_ski_length(skier):
     if skier.age <= 4:
-        return (skier.length, skier.length)
+        return _calculate_baby_ski_length(skier)
     if skier.age <= 8:
-        return (skier.length + 10, skier.length + 20)
+        return _calculate_child_ski_length(skier)
     if skier.style == Skier.classic:
-        return (skier.length + 20, skier.length + 20)
-    if skier.style == Skier.free_style:
-        return (skier.length + 10, skier.length + 15)
+        return _calculate_classic_ski_length(skier)
+    if skier.style == Skier.freestyle:
+        return _calculate_freestyle_ski_length(skier)
+    raise ValueError("Unsupported ski style")
+
+def _calculate_baby_ski_length(skier):
+    return (skier.length, skier.length)
+
+def _calculate_child_ski_length(skier):
+    return (skier.length + 10, skier.length + 20)
+
+def _calculate_classic_ski_length(skier):
+    ski_length = (skier.length + 20, skier.length + 20)
+    if ski_length[1] > _MAX_CLASSIC_SKI_LENGTH:
+        raise ValueError(f"Classic ski length is {ski_length},"
+                          "max supported length is {_MAX_CLASSIC_SKI_LENGTH}")
+    return ski_length
+
+def _calculate_freestyle_ski_length(skier):
+    return (skier.length + 10, skier.length + 15)
--- tests/test_ski_calculator.py
+++ tests/test_ski_calculator.py
@@ -5,11 +5,23 @@ from ski_calculator.calculator import Skier, calculate_ski_length
 @pytest.mark.parametrize('length, age, style, expected', [
         (80,     4,   Skier.classic,    (80, 80)),
         (80,     5,   Skier.classic,    (90, 100)),
-        (80,     10,  Skier.classic,    (100, 100)),
-        (80,     10,  Skier.free_style, (90, 95)),
+        (140,     9,  Skier.classic,   (160, 160)),
+        (187,     30,  Skier.classic,   (207, 207)),
+        (180,     30,  Skier.freestyle, (190, 195)),
 ])
 
 def test_calculate_ski_length(length, age, style, expected):
     skier = Skier(length, age, style)
     ski_length = calculate_ski_length(skier)
     assert expected == ski_length
+
+
+@pytest.mark.parametrize('length, age, style', [
+        (198,     30,   Skier.classic), # Skier too long
+        (180,     30,   "Invalid Ski Type"),
+])
+
+def test_calculate_ski_length_value_error(length, age, style):
+    skier = Skier(length, age, style)
+    with pytest.raises(ValueError):
+        ski_length = calculate_ski_length(skier)
