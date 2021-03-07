30743dfec137621734473f3cb435cafcdc374f09
Sebastian <riktig@users.noreply.github.com>
2021-03-07 21:08:43 +0100

    Add command line argument parser
    add option to specify port

--- ski_calculator/main.py
+++ ski_calculator/main.py
@@ -1,10 +1,20 @@
 from http.server import HTTPServer
+from argparse import ArgumentParser
 
 from .server import SkiRequestHandler
 
 
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('-p', "--port", help="Specify alternate port [default: 8000]", type=int, default=8000)
+
+    return parser.parse_args()
+
+
 def main():
-    server = HTTPServer(("localhost", 8000), SkiRequestHandler)
+    args = get_args()
+
+    server = HTTPServer(("localhost", args.port), SkiRequestHandler)
     server.serve_forever()
 
 
