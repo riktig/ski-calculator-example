6b01576fb56fad6b28f3cdbd9d7be6194b66df0b
Sebastian <riktig@users.noreply.github.com>
2021-03-07 01:28:54 +0100

    Add webserver for ski length post request

--- ski_calculator/main.py
+++ ski_calculator/main.py
@@ -1,7 +1,47 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
 from .calculator import Skier, calculate_ski_length
 
+
+class SkiRequestHandler(BaseHTTPRequestHandler):
+    def _send_success_response(self, response):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(response.encode('utf-8'))
+
+    def _get_post_data(self):
+        content_length = int(self.headers['Content-Length'])
+        return self.rfile.read(content_length)
+
+    def do_POST(self):
+        post_data = self._get_post_data()
+        json_data = load_json(post_data)
+
+        skier = create_skier(json_data)
+        ski_length = calculate_ski_length(skier)
+
+        response = create_response_json(ski_length)
+        self._send_success_response(response)
+
+
+def load_json(data):
+    return json.loads(data)
+
+
+def create_skier(json_data):
+    return Skier(json_data["length"], json_data["age"], json_data["style"])
+
+
+def create_response_json(ski_length):
+    return json.dumps({'ski_length': ski_length})
+
+
 def main():
-    print("Hello World")
+    server = HTTPServer(("localhost", 8000), SkiRequestHandler)
+    server.serve_forever()
+
 
 if __name__ == "__main__":
     main()
